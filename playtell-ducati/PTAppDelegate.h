b6021db760a1263f60d22b0aecf9a97a8662b3f4
Ricky Hussmann <ricky.hussmann@gmail.com>
2012-05-30 16:07:49 -0400

    Initial Commit

---
+++ playtell-ducati/PTAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  PTAppDelegate.h
+//  playtell-ducati
+//
+//  Created by Ricky Hussmann on 5/30/12.
+//  Copyright (c) 2012 LovelyRide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class PTViewController;
+
+@interface PTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) PTViewController *viewController;
+
+@end
---
+++ playtell-ducati/PTViewController.h
@@ -0,0 +1,13 @@
+//
+//  PTViewController.h
+//  playtell-ducati
+//
+//  Created by Ricky Hussmann on 5/30/12.
+//  Copyright (c) 2012 LovelyRide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTViewController : UIViewController
+
+@end
