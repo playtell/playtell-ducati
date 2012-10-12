1553f92fa210746dcc072a85bc8407cdfd52e9d9
Adam Horne <ahorne@wordplaysoftware.com>
2012-10-12 15:03:43 +0100

    Merged changes

--- playtell-ducati/views/PTChatHUDView2.m
+++ playtell-ducati/views/PTChatHUDView2.m
@@ -134,12 +134,9 @@
 }
 
 - (void)setRightView:(UIView*)aView {
-<<<<<<< HEAD
     // Set new view's frame
-=======
     [aView removeAllGestureRecognizers];
     [self.rightContainerView removeAllSubviews];
->>>>>>> c55252a386a1aa0af29311f9e7e8af8a1f1a936e
     aView.frame = [[self class] rectForRightSubview];
     
     // Fadeout current subviews
